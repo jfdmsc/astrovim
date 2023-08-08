4245bebb411ffd899c006d7bc10f025a0283e77c
jfdmsc <105931223+jfdmsc@users.noreply.github.com>
2023-08-07 23:05:47 -0400

    start to change thins

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -7,7 +7,18 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "lua_ls",
+        "lua_ls",
+        "astro",
+        "cssls",
+        "dockerls",
+        "nil_ls",
+        "yamlls",
+        "docker_compose_language_service",
+        "tailwindcss",
+        "ansiblels",
+        "arduino_language_server",
+        "html",
+        "tsserver",
       })
     end,
   },
@@ -18,8 +29,9 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "prettier",
-        -- "stylua",
+        "prettier",
+        "stylua",
+        "luacheck",
       })
     end,
   },
